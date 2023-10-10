9394b125535760d62ae0c20e3638fb20346d8693
hendricksonweib <hendricksonrocha22@gmail.com>
2023-10-10 08:58:16 -0300

    :lipstick: feat repos-list
    Signed-off-by: hendricksonweib <hendricksonrocha22@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,21 +1,36 @@
-function oneClick(){
+function oneClick() {
     const username = document.querySelector(".search").value
     search(username)
-    console.log(username)
-}
-async function search(username){    
-    if(username == ''){
-    alert("digite um usuário")
-} else {
-    const dados = await fetch(`https://api.github.com/users/${username}`).then(response => response.json())
-    .then((data) => {
-        document.getElementById("userPicture").src = data.avatar_url
-        document.querySelector("h2").textContent = data.login
-        document.querySelector("a").href = data.html_url
-        document.querySelector("a").textContent = data.html_url
-        document.querySelector("p").textContent = data.bio || "No bio available"
-        document.querySelector("h4").textContent = data.gists_url
-    }) 
-}
-}
-
+  }
+  
+  async function search(username) {
+    if (username == "") {
+      alert("Digite um usuário")
+    } else {
+      const expandir = document.getElementById("main")
+      expandir.style.display = "flex"
+      const userData = await fetch(`https://api.github.com/users/${username}`).then(
+        (response) => response.json()
+      )
+      document.getElementById("userPicture").src = userData.avatar_url
+      document.querySelector("h2").textContent = userData.login
+      document.querySelector("a").href = userData.html_url
+      document.querySelector("a").textContent = userData.html_url
+      document.querySelector("p").textContent = userData.bio || "No bio available"
+  
+   
+      const reposData = await fetch(`https://api.github.com/users/${username}/repos`).then(
+        (response) => response.json()
+      )
+  
+      const reposList = document.getElementById("repos-list")
+      reposList.innerHTML = ""
+  
+      reposData.forEach((repo) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a>`
+        reposList.appendChild(listItem)
+      })
+    }
+  }
+  
